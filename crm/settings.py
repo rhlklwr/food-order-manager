cd14bb175c6de6002c806a18fd6e3fecbc5b0da5
rhlklwr <kalwar.rahul@gmail.com>
2020-12-19 22:47:36 +0530

    issue with profile pic

--- crm/settings.py
+++ crm/settings.py
@@ -121,7 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = Path(BASE_DIR).joinpath('staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
