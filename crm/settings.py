a1f2b625129d59363537beab8ce110dd84b1fd50
rhlklwr <kalwar.rahul@gmail.com>
2021-03-16 16:44:51 +0530

    Rollbacked 2 commits

--- crm/settings.py
+++ crm/settings.py
@@ -120,17 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = Path(BASE_DIR).joinpath('staticfiles')
+
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     Path(BASE_DIR).joinpath('static')
 ]
-
-STATIC_ROOT = Path(BASE_DIR).joinpath('static')
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-MEDIA_URL = '/images/'
 
 MEDIA_ROOT = Path(BASE_DIR).joinpath('static/images')
 
