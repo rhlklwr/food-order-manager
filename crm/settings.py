54c3918d0b00d6e7c3ecb03989e2d045bf1e77ac
rhlklwr <kalwar.rahul@gmail.com>
2021-03-16 16:46:55 +0530

    updated media root

--- crm/settings.py
+++ crm/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
     Path(BASE_DIR).joinpath('static')
 ]
 
-MEDIA_ROOT = Path(BASE_DIR).joinpath('static/images')
+MEDIA_ROOT = Path(BASE_DIR).joinpath('staticfiles/images')
 
 
 # SMTP configuration
